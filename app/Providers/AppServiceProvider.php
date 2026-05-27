5911011a326466ced813756abb69e6e8d085472c
Victor Hugo <L20271015@tuxtla.tecnm.mx>
2026-05-26 22:57:31 -0600

    fix: forzar esquema https para los assets de vite

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // <-- Asegúrate de importar esta fachada
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Fuerza el uso de HTTPS si la aplicación está en producción
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
